b174239911d79155d31d6381698e6444e6b3b692
PierreP <53476143+RoYaLSleT@users.noreply.github.com>
2025-09-04 11:18:30 +0200

    Article creation op with validation

--- src/app/article-create/article-create.ts
+++ src/app/article-create/article-create.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Article } from '../common/article.interface';
 import { ArticlesService } from '../common/articles-service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-article-create',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './article-create.html',
   styleUrl: './article-create.css',
 })
@@ -16,13 +18,28 @@ export class ArticleCreate {
     userId: 0,
   };
 
-  constructor(private articleService: ArticlesService) {}
+  constructor(
+    private articleService: ArticlesService,
+    private toastr: ToastrService,
+    private routeur: Router
+  ) {}
 
   createArticle(): void {
-    this.articleService
-      .createArticle(this.article)
-      .subscribe
-      // to be completed
-      ();
+    this.articleService.createArticle(this.article).subscribe({
+      next: () => {
+        // On prévient l'utilisateur
+        this.toastr.success('Article bien ajouté !');
+        // on redirige vers la liste des articles
+        this.routeur.navigate(['/']);
+      },
+      error: (err) => {
+        console.error(err);
+        // TODO : gérer les différentes erreurs
+        // Si erreur de backend (500), on a juste à dire à l'utilisateur qu'il ya un pépin, on ne peut pas accéder à sa demande.
+        // On peut avoir un problème de validation des données cotés serveur
+        this.toastr.error("Erreur durant la création de l'article");
+        this.routeur.navigate(['/']);
+      },
+    });
   }
 }
